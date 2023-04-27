e0647a8a29ef156c5ab0310a1c1a1c94b95c2518
Massimiliano Gasaro <massimilianogasaro95@gmail.com>
2023-04-27 17:10:03 +0200

    create persistant login

--- angularapp/src/app/components/login/login.component.ts
+++ angularapp/src/app/components/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { UserLogin } from 'src/app/models/login';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -8,23 +10,35 @@ import { FormControl, FormGroup } from '@angular/forms';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
   }
 
   form: FormGroup = new FormGroup({
-    username: new FormControl(''),
-    password: new FormControl(''),
+    username: new FormControl("", Validators.compose([Validators.required])),
+    password: new FormControl("", Validators.compose([Validators.required])),
   });
 
-  submit() {
-    if (this.form.valid) {
-      this.submitEM.emit(this.form.value);
+  public login() {
+    if(this.form.valid) {
+      console.log(this.form); // debug
+      let formValue = this.form.value;
+      let user = new UserLogin(formValue.username, formValue.password);
+      this.authService.login(user).subscribe({
+        next: result => {
+          console.log(result); // debug
+        },
+        error: err => console.log(err),
+        complete: () => console.log("chiamata completata") // debug
+      })
     }
+    // if (this.form.valid) {
+    //   this.submitEM.emit(this.form.value);
+    // }
   }
   // error: string | null;
 
-  @Output() submitEM = new EventEmitter();
+  // @Output() submitEM = new EventEmitter();
 
 }
--- angularapp/src/app/services/auth.service.ts
+++ angularapp/src/app/services/auth.service.ts
@@ -1,25 +1,58 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, map} from 'rxjs';
 import { Observable } from 'rxjs/internal/Observable';
+import { UserLogin } from '../models/login';
 import { User } from '../models/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+
+  private baseUrl: string = "https://localhost:7009/api/";
+
+  private currentUserSource = new BehaviorSubject<User | null>(null);
+  currentUser$ = this.currentUserSource.asObservable();
+
   constructor(private http: HttpClient) {}
 
+  /**
+   * login
+   * @param user
+   * @returns
+   */
+  public login(userLogin: UserLogin) {
+    return this.http.post<User>(`${this.baseUrl}auth/login`, userLogin).pipe(
+      map((response: User) => {
+        const user = response;
+        if(user) {
+          localStorage.setItem("user", JSON.stringify(user));
+          this.currentUserSource.next(user);
+        }
+      })
+    );
+  }
+
+  public setCurrentUser(user: User) {
+    this.currentUserSource.next(user);
+  }
+
+  public logout() {
+    localStorage.removeItem("user");
+    this.currentUserSource.next(null);
+  }
+
+  /**
+   * register
+   * @param user
+   * @returns
+   */
   public register(user: User): Observable<any> {
     return this.http.post<any>(
       'https://localhost:7009/api/Auth/register',
       user
     );
-  }
-
-  public login(user: User): Observable<string> {
-    return this.http.post('https://localhost:7009/api/Auth/login', user, {
-      responseType: 'text',
-    });
   }
 
   public getMe(): Observable<string> {
